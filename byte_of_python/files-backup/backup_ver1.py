74facef6050663184330cadcbfad53369da1ecf0
Henri Kotka <henri.kotka@gmail.com>
2017-08-23 20:39:18 +0300

    Converted into linux format

--- byte_of_python/files-backup/backup_ver1.py
+++ byte_of_python/files-backup/backup_ver1.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+
 import os
 import time
 
-source = ['C:\\Temp', 'C:\\Temp2']
+source = ['/home/henkka/files', '/home/henkka/files2']
 
-target_dir = 'C:\\backup'
+target_dir = '/home/henkka/backup'
 
 target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
 
-zip_command = "zip -qr {0} {1}".format(target, ''.join(source))
+zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
 
 if os.system(zip_command) == 0:
     print('Successful backup to', target)
