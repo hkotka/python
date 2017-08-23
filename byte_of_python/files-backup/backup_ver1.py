bdf4d97beac6e0a559f1e6d506c321b4a93ca4a9
Henri Kotka <henri.kotka@gmail.com>
2017-08-23 20:53:38 +0300

    version 2

--- byte_of_python/files-backup/backup_ver1.py
+++ byte_of_python/files-backup/backup_ver1.py
@@ -7,7 +7,16 @@ source = ['/home/henkka/files', '/home/henkka/files2']
 
 target_dir = '/home/henkka/backup'
 
-target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+now = time.strftime('%H%M%S')
+
+# Create the subdirectory if it isn‚t already there
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Successfully created directory', today)
+
+# The name of the zip file
+target = today + os.sep + now + '.zip'
 
 zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
 
