75610aa1715897e40328e11019b41f708c805381
Henkka <henkka@localhost.localdomain>
2017-11-16 23:46:29 +0200

    Simplified if

--- byte_of_python/files-backup/backup.py
+++ byte_of_python/files-backup/backup.py
@@ -3,7 +3,7 @@ import os
 import time
 from sys import argv
 
-source = ['/home/henkka/files', '/home/henkka/files2']
+source = ['/home/henkka/backup/files1', '/home/henkka/backup/files2']
 
 target_dir = '/home/henkka/backup'
 
@@ -12,7 +12,7 @@ now = time.strftime('%H%M%S')
 
 # Take the user input comment as a name of the zip file
 comment = input('Enter a comment -->')
-if len(comment) == 0: # check if comment was entered
+if comment == 0: # check if comment was entered
     target = today + os.sep + now + '.zip'
 else:
     target = today + os.sep + now + '_' + comment.replace(' ', '_') + '.zip'
@@ -25,15 +25,15 @@ if not os.path.exists(today):
 # User can pass additional source folders for backup as arguments
 if len(argv) > 1:
     for arg in argv[1:]:
-       if arg != '-v':
-          source.append(arg)
+        if arg != '-v':
+            source.append(arg)
 
 # Run zip program with verbose output if the user passes -v argument
 if len(argv) > 1:
     if argv[1] == '-v':
-       zip_command = "zip -rv {0} {1}".format(target, ' '.join(source))
+        zip_command = "zip -rv {0} {1}".format(target, ' '.join(source))
     elif argv[1] != '-v':
-       zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
+        zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
 else:
     zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
 
@@ -41,4 +41,3 @@ if os.system(zip_command) == 0:
     print('Successful backup to', target)
 else:
     print('Backup FAILED')
-
