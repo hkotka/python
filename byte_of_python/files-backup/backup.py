6fe648d8868a8bd54dca43ad1305bdad88f698b2
Henkka <henkka@localhost.localdomain>
2018-04-18 17:03:12 +0300

    - Improved the handling of commandline arguments - Added support for adding more directories into backup sources - Added support for displaying program help message with --help arg

--- byte_of_python/files-backup/backup.py
+++ byte_of_python/files-backup/backup.py
@@ -2,13 +2,34 @@
 import os
 import time
 from sys import argv
+import func_help
 
 source = ['/home/henkka/backup/files1', '/home/henkka/backup/files2']
-
 target_dir = '/home/henkka/backup'
 
 today = target_dir + os.sep + time.strftime('%Y%m%d')
 now = time.strftime('%H%M%S')
+
+zip_command = "zip -qr "
+
+# User can pass additional commandline arguments for different options
+if len(argv) > 0:
+    i = 0
+# Display program help message
+    for arg in argv:
+        if arg == '--help':
+            func_help.helpMsg()
+            break
+# Add directories into additional sources for backup
+        elif arg == '-d':
+            if i+2 <= len(argv):
+                source.append(argv[i+1])
+            else:
+                break
+# Run zip program with verbose output if the user passes -v argument
+        elif arg == '-v':
+            zip_command = "zip -rv "
+        i += 1
 
 # Take the user input comment as a name of the zip file
 comment = input('Enter a comment -->')
@@ -17,27 +38,14 @@ if comment == 0: # check if comment was entered
 else:
     target = today + os.sep + now + '_' + comment.replace(' ', '_') + '.zip'
 
+zip_commandFinal = zip_command+"{0} {1}".format(target, ' '.join(source))
+
 # Create the subdirectory if it isn‚t already there
 if not os.path.exists(today):
     os.mkdir(today)
     print('Successfully created directory', today)
 
-# User can pass additional source folders for backup as arguments
-if len(argv) > 1:
-    for arg in argv:
-        if arg != '-v':
-            source.append(arg)
-
-# Run zip program with verbose output if the user passes -v argument
-if len(argv) > 1:
-    if argv[1] == '-v':
-        zip_command = "zip -rv {0} {1}".format(target, ' '.join(source))
-    elif argv[1] != '-v':
-        zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
-else:
-    zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
-
-if os.system(zip_command) == 0:
+if os.system(zip_commandFinal) == 0:
     print('Successful backup to', target)
 else:
     print('Backup FAILED')
