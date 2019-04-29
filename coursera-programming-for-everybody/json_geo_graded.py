0116299a8342b763604179e13007bf491b231f98
Henri Kotka <henri.kotka@gmail.com>
2019-04-29 21:53:39 +0300

    Removed ssl ignore feature and changed url to https

--- coursera-programming-for-everybody/json_geo_graded.py
+++ coursera-programming-for-everybody/json_geo_graded.py
@@ -9,14 +9,10 @@ api_key = False
 
 if api_key is False:
     api_key = 42
-    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+    serviceurl = 'https://py4e-data.dr-chuck.net/json?'
 else :
     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
 
 while True:
     address = input('Enter location: ')
@@ -27,7 +23,7 @@ while True:
     if api_key is not False: parms['key'] = api_key
     url = serviceurl + urllib.parse.urlencode(parms)
 
-    uh = urllib.request.urlopen(url, context=ctx)
+    uh = urllib.request.urlopen(url)
     data = uh.read().decode()
 
     try:
