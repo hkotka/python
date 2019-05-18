68bbceba4d2fa8a8aa6d641534ee970eb60c7aca
Henri Kotka <henri.kotka@gmail.com>
2019-05-18 10:21:28 +0200

    Removed unnessecary ssl context settings

--- coursera-programming-for-everybody/json_graded1.py
+++ coursera-programming-for-everybody/json_graded1.py
@@ -2,14 +2,9 @@ import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-
 # Retrieve json data into Python dictionary
 url = "http://py4e-data.dr-chuck.net/comments_147422.json"
-handle = urllib.request.urlopen(url, context=ctx)
+handle = urllib.request.urlopen(url)
 data = handle.read().decode()
 data = json.loads(data)
 
